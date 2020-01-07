44bcb01a7ef17a7404915c72b42c1c5ad8ff9916
Prince Sumberia <broughtmeup@gmail.com>
2020-01-08 00:28:13 +0530

    added changing emoji

--- src/Joke.js
+++ src/Joke.js
@@ -9,17 +9,19 @@ export default class Joke extends Component {
 	handleClick(evt) {
 		this.props.updateScore(evt.target.id, this.props.id);
 	}
+
 	render() {
 		return (
 			<div>
-				<button id="upVote" onClick={this.handleClick}>
+				<button id="up-vote" onClick={this.handleClick}>
 					+
 				</button>
 				{this.props.score}
-				<button id="downVote" onClick={this.handleClick}>
+				<button id="down-vote" onClick={this.handleClick}>
 					-
 				</button>
 				{this.props.joke}
+				{this.props.emoji}
 			</div>
 		);
 	}
--- src/JokeBoard.js
+++ src/JokeBoard.js
@@ -7,28 +7,81 @@ export default class JokeBoard extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { jokes: [] };
-		this.getJoke = this.getJoke.bind(this);
+		this.getJokes = this.getJokes.bind(this);
 		this.updateScore = this.updateScore.bind(this);
+		this.setEmoji = this.setEmoji.bind(this);
 	}
 
-	async getJoke() {
-		const response = await axios.get('https://icanhazdadjoke.com/slack');
-		const newJoke = response.data.attachments[0].text;
+	componentDidMount() {
+		this.getJokes();
+	}
+
+	async getJokes() {
+		let newJokes = [];
+		for (let index = 0; index < 10; index++) {
+			const response = await axios.get('https://icanhazdadjoke.com/slack');
+			newJokes = [ ...newJokes, { id: uuid(), text: response.data.attachments[0].text, score: 0, emoji: '😀' } ];
+		}
 		this.setState((st) => ({
-			jokes: [ ...st.jokes, { id: uuid(), text: newJoke, score: 0 } ]
+			jokes: [ ...st.jokes, ...newJokes ]
 		}));
 	}
+
 	updateScore(vote, id) {
-		console.log(vote, id);
+		const updatedJokes = this.state.jokes.map((joke) => {
+			if (joke.id === id) {
+				if (vote === 'up-vote') {
+					joke.score += 1;
+				} else {
+					joke.score -= 1;
+				}
+				joke.emoji = this.setEmoji(joke.score);
+				console.log(joke.score);
+				console.log(joke.emoji);
+			}
+			return joke;
+		});
+		this.setState({ jokes: updatedJokes });
 	}
+
+	setEmoji(score) {
+		console.log(score);
+		let emoji;
+		switch (true) {
+			case score > 1 && score <= 2:
+				emoji = '😁';
+				break;
+			case score > 2 && score <= 5:
+				emoji = '😅';
+				break;
+			case score > 5 && score <= 10:
+				emoji = '😂';
+				break;
+			case score > 10:
+				emoji = '🤣';
+				break;
+			default:
+				emoji = '😀';
+				break;
+		}
+		return emoji;
+	}
+
 	render() {
 		const jokes = this.state.jokes.map((joke) => (
-			<Joke key={joke.id} id={joke.id} joke={joke.text} updateScore={this.updateScore} />
+			<Joke
+				key={joke.id}
+				id={joke.id}
+				joke={joke.text}
+				score={joke.score}
+				emoji={joke.emoji}
+				updateScore={this.updateScore}
+			/>
 		));
 		return (
 			<div>
 				{jokes}
-				<button onClick={this.getJoke}>Dad Joke!</button>
+				<button onClick={this.getJokes}>New Jokes!</button>
 			</div>
 		);
 	}
