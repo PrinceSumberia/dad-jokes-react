2be1d97262afff37b33e150326404cc275f9cbf9
Prince Sumberia <broughtmeup@gmail.com>
2020-01-07 23:20:30 +0530

    added upvote and downvote buttons

--- src/Joke.js
+++ src/Joke.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 export default class Joke extends Component {
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(evt) {
+		this.props.updateScore(evt.target.id, this.props.id);
+	}
 	render() {
 		return (
 			<div>
-				<p>{this.props.joke}</p>
+				<button id="upVote" onClick={this.handleClick}>
+					+
+				</button>
+				{this.props.score}
+				<button id="downVote" onClick={this.handleClick}>
+					-
+				</button>
+				{this.props.joke}
 			</div>
 		);
 	}
--- src/JokeBoard.js
+++ src/JokeBoard.js
@@ -8,16 +8,23 @@ export default class JokeBoard extends Component {
 		super(props);
 		this.state = { jokes: [] };
 		this.getJoke = this.getJoke.bind(this);
+		this.updateScore = this.updateScore.bind(this);
 	}
+
 	async getJoke() {
 		const response = await axios.get('https://icanhazdadjoke.com/slack');
 		const newJoke = response.data.attachments[0].text;
 		this.setState((st) => ({
-			jokes: [ ...st.jokes, { id: uuid(), text: newJoke } ]
+			jokes: [ ...st.jokes, { id: uuid(), text: newJoke, score: 0 } ]
 		}));
 	}
+	updateScore(vote, id) {
+		console.log(vote, id);
+	}
 	render() {
-		const jokes = this.state.jokes.map((joke) => <Joke key={joke.id} joke={joke.text} />);
+		const jokes = this.state.jokes.map((joke) => (
+			<Joke key={joke.id} id={joke.id} joke={joke.text} updateScore={this.updateScore} />
+		));
 		return (
 			<div>
 				{jokes}
